229b3f4fab4a32db5f56c55a78c21dcc54cc98d6
Jean-Chrisrophe Ozanne <jeanchristopheozanne@gmail.com>
2025-04-19 15:07:47 +0200

    refactorisation des templates, vues et routes pour les nouvelles applications lettings et profiles

--- lettings/views.py
+++ lettings/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Letting
 
-# Create your views here.
+def index(request):  # ancien lettings_index
+    lettings_list = Letting.objects.all()
+    context = {'lettings_list': lettings_list}
+    return render(request, 'lettings/index.html', context)
+
+def letting(request, letting_id):
+    letting = get_object_or_404(Letting, id=letting_id)
+    context = {
+        'letting': letting,
+    }
+    return render(request, 'lettings/letting.html', context)
--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Profile
 
-# Create your views here.
+def index(request):  # ancien profiles_index
+    profiles_list = Profile.objects.all()
+    context = {'profiles_list': profiles_list}
+    return render(request, 'profiles/index.html', context)
+
+def profile(request, username):
+    profile = get_object_or_404(Profile, user__username=username)
+    context = {'profile': profile}
+    return render(request, 'profiles/profile.html', context)
