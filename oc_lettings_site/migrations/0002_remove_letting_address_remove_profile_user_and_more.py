4483bbe09e439e4b1397aaef92da3cf3af095646
Jean-Chrisrophe Ozanne <jeanchristopheozanne@gmail.com>
2025-04-30 13:49:19 +0200

    test ci

--- oc_lettings_site/migrations/0002_remove_letting_address_remove_profile_user_and_more.py
+++ oc_lettings_site/migrations/0002_remove_letting_address_remove_profile_user_and_more.py
@@ -1,6 +1,11 @@
 from django.db import migrations
 
 class Migration(migrations.Migration):
+    """
+    A migration to remove the Address, Letting, and Profile models.
+    This migration removes the Address, Letting, and Profile models
+    from the database and application state.
+    """
 
     dependencies = [
         ('oc_lettings_site', '0001_initial'),
