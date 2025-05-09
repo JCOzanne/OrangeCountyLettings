d4e4bbf72e7c2739474438905b94b0c4ff95db68
Jean-Chrisrophe Ozanne <jeanchristopheozanne@gmail.com>
2025-05-09 14:05:08 +0200

    modification du fichier start.sh suite erreur déploiement

--- start.sh
+++ start.sh
@@ -2,6 +2,6 @@
 
 PORT=${PORT:-8000}
 
-python manage.py migrate
-python manage.py collectstatic --noinput
+python manage.py migrate && \
+python manage.py collectstatic --noinput && \
 gunicorn oc_lettings_site.wsgi:application --bind 0.0.0.0:$PORT
